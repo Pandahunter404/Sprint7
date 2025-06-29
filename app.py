6f82bd6448f26ea6fca6f7a41717e46cbe99fc50
Pandahunter404 <ireri-ac@hotmail.com>
2025-06-28 19:18:31 -0600

    print nuevo

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-print("Kika summer")
-print("yo vi a tu novio en grindr")
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+car_data = pd.read_csv('C:\Users\bakut\Documents\Proyectos\Sprint7\vehicles_us.csv') # leer los datos
+fig = px.histogram(car_data, x="odometer") # crear un histograma
+fig.show() # crear gráfico de dispersión
